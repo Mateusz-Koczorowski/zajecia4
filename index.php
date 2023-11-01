da4f7260b78896886e583737d9d91532b46bb02b
Mateusz Koczorowski <45535816+Mateusz-Koczorowski@users.noreply.github.com>
2023-11-01 10:10:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:mkoczorowskidbwdco.database.windows.net,1433; Database = zajecia4", "mkoczorowski", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:mkoczorowskibaza.database.windows.net,1433; Database = zajecia4baza", "CloudSAef5d9c88", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "mkoczorowski", "pwd" => "{your_password_here}", "Database" => "zajecia4", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:mkoczorowskidbwdco.database.windows.net,1433";
+$connectionInfo = array("UID" => "CloudSAef5d9c88", "pwd" => "{your_password_here}", "Database" => "zajecia4baza", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:mkoczorowskibaza.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
