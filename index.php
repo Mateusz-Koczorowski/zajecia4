0266a1eb580c52cc567cc8fdfca9b316f39370ef
Mateusz Koczorowski <45535816+Mateusz-Koczorowski@users.noreply.github.com>
2023-11-01 10:29:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-echo "Dupa";
+<?php
+$host = "zajecia4mateuszkoczorowski-server.postgres.database.azure.com";
+$port = 5432;
+$dbname = "postgres";
+$user = "lascudaule";
+$password = "twoje_haslo";
+
+$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+if (!$connection) {
+    die("Błąd połączenia z bazą danych: " . pg_last_error());
+} else {
+    echo "Połączono z bazą danych PostgreSQL.";
+    // Możesz wykonywać operacje na bazie danych tutaj.
+}
+?>
