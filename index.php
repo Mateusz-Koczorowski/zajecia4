f264875754e95a0138540be42ee3464a21732a64
Mateusz Koczorowski <45535816+Mateusz-Koczorowski@users.noreply.github.com>
2023-11-01 10:18:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
-<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:mkoczorowskibaza.database.windows.net,1433; Database = zajecia4baza", "CloudSAef5d9c88", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+$host = "zajecia4mateuszkoczorowski-server.postgres.database.azure.com";
+$port = 5432;
+$dbname = "postgres";
+$user = "lascudaule";
+$password = "serwer2023!";
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "CloudSAef5d9c88", "pwd" => "{your_password_here}", "Database" => "zajecia4baza", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:mkoczorowskibaza.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-?>
+$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+if (!$connection) {
+    die("Błąd połączenia z bazą danych: " . pg_last_error());
+} else {
+    echo "Połączono z bazą danych PostgreSQL.";
+    // Możesz wykonywać operacje na bazie danych tutaj.
+}
